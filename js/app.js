b5744aa7d3c5fa727d8882cc09d3d17fec5cc001
Gonzalo Tey <gontey@hotmail.com>
2022-07-28 12:14:35 -0300

    Slider completo - Productos completo

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
-//Menu Responsive
+//Encabezado Responsive
 
 const btnMenuOpen = document.getElementById("btnMenuOpen");
 const btnMenuClose = document.getElementById("btnMenuClose");
 const menuResponsive = document.getElementById("menu-bar");
 const enlaces = document.getElementById("enlaces");
 
-//Evento Abrir Menu
+      //Evento Abrir Menu
 
 btnMenuOpen.addEventListener("click", () => {
     menuResponsive.classList.add("active");
@@ -15,9 +15,27 @@ btnMenuClose.addEventListener("click", () => {
     menuResponsive.classList.remove("active");
 });
 
-//Cerrar Menu con elementos de enlace
+      //Cerrar Menu con elementos de enlace
 
 enlaces.addEventListener("click", () => {
     menuResponsive.style.transitionDelay = ".2s";
     menuResponsive.classList.remove("active");
+});
+
+
+//Slider de productos
+
+const contenedor = document.querySelector('.slider');
+const btnIzquierda = document.getElementById("btnIzquierda");
+const btnDerecha = document.getElementById("btnDerecha");
+
+     //Evento Boton Derecho
+
+btnDerecha.addEventListener("click", () => {
+    contenedor.scrollLeft += contenedor.offsetWidth;
+});
+     //Evento Boton Izquierdo
+
+btnIzquierda.addEventListener("click", () => {
+    contenedor.scrollLeft -= contenedor.offsetWidth;
 });
